0fd5f18bfcbcb2109c17bbb990b9aec04ddfaf02
maggie kaminska <marg.adm.speak@gmail.com>
2023-04-09 00:17:51 +0100

    add product card functions

--- src/Components/ProductsSection/ProductsSection.js
+++ src/Components/ProductsSection/ProductsSection.js
@@ -1,15 +1,45 @@
 import "./ProductsSection.scss";
+import { BsArrowLeftShort } from "react-icons/bs";
+import { BsArrowRightShort } from "react-icons/bs";
+import pic from "../../assets/images/heroImg.jpg";
 
 const ProductsSection = () => {
   return (
     <>
       <section className="products">
         <div className="products__container">
-          <div className="products__flex">
+          <div className="products__header-flex">
             <div className="products__text">
               <h2>Our products</h2>
+              <p>remove fsdfdsfdfdsf</p>
             </div>
-            <p>remove</p>
+            <div className="products__icons-container">
+              <BsArrowLeftShort className="products__icon" />
+              <BsArrowRightShort className="products__icon" />
+            </div>
+          </div>
+
+          <div className="products__main">
+            <div className="products__single">
+              <div className="products__img-container">
+                <img className="products__img" src={pic} alt="knit" />
+
+                <div className="products__overlay-info">
+                  <h3 className="products__heading">Some text</h3>
+                  <p className="products__overlay-text">lorem ipsum</p>
+                  <BsArrowRightShort className="products__overlay-icon" />
+                </div>
+              </div>
+              <div className="products__single-footer">
+                <div className="products__number">01</div>
+                <div className="products__info-flex">
+                  <h6 className="products__info-heading">London</h6>
+                  <span className="products__flex">
+                    <span className="products__dot">dot icon</span>
+                  </span>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </section>
